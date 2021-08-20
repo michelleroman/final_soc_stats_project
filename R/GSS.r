da7687cf8481916e4b4b89afa1f50a67c2da2083
Michelle Roman <micheller@uaspire.org>
2021-08-20 14:26:37 -0400

    renamed data frame, rewrote distribution viewing and factor creation

--- R/GSS.r
+++ R/GSS.r
@@ -1,63 +1,102 @@
+## Cleaning script to process General Social Survey data and prepare for analysis
 
 # Load packages
-
 library(here)
 library(janitor)
 library(readr)
 library(dplyr)
+library(ggplot2)
+library(forcats)
 
 # Establish working directory
 input_path <- here::here("input")
-
 setwd(input_path)
-
 
 # Identified metadata DCT file
 metadata <- readLines("GSS.dct")
 print(metadata)
 
 # Load General Social Survey data
-GSS <-
-  readr::read_fwf(
-    "GSS.dat",
-    col_positions = fwf_positions(
-      start = c(1, 21, 41, 61, 81),
-      end = c(20, 40, 60, 80, 100),
-      col_names = c("YEAR", "ID_", "HEALTH", "CLASS", "HELPSICK")
-  )) %>%
+# WE are pulling the entire dataset going back to 1972
+raw_df <-
+  readr::read_fwf("GSS.dat",
+                  col_positions = fwf_positions(
+                    start = c(1, 21, 41, 61, 81),
+                    end = c(20, 40, 60, 80, 100),
+                    col_names = c("YEAR", "ID_", "HEALTH", "CLASS", "HELPSICK")
+                  )) %>%
   janitor::clean_names()
 
 ## Review distribution of responses
-table(GSS$class, exclude= NULL)
-table(GSS$health, exclude= NULL)
-table(GSS$helpsick, exclude= NULL)
 
-## Set blank or outlier values to NULL/NA
-## Drop answers with fewer than 1000 rows
-## Drop 0 since it is equivalent to NA
+#Subjective class identification
+ggplot2::ggplot(data = raw_df) +
+  ggplot2::geom_bar(mapping = aes(x = class))
 
-#GSS$class[GSS$class>=5] <- NA
-#GSS$class[GSS$class==0] <- NA
+#Condition of health
+ggplot2::ggplot(data = raw_df) +
+  ggplot2::geom_bar(mapping = aes(x = health))
 
-#GSS$health[GSS$health>=8] <- NA
-#GSS$health[GSS$health==0] <- NA
+#Should govt help pay for medical care
+ggplot2::ggplot(data = raw_df) +
+  ggplot2::geom_bar(mapping = aes(x = helpsick))
 
-GSS$helpsick[GSS$helpsick>=8|GSS$helpsick==0] <- NA
 
-table(GSS$class, exclude= NULL)
-table(GSS$health, exclude= NULL)
-table(GSS$helpsick, exclude= NULL)
+## We want to ensure that we have enough of a sample size to
+## draw conclusions around the relationships between these
+## variables.
 
-## Recode variables 
+# 0 values represent Not applicable and responses that are
+# greater than 5 are equivalent to Don't know or No answer, we can safely recode
+# these rows  to NULL since they are not relevant to our analysis.
 
+# Recode outliers and null values
+df <- raw_df %>%
+  mutate(class = dplyr::if_else((class >= 5 |
+                                   class == 0), NA_real_, class)) %>%
+  mutate(health = dplyr::if_else((health >= 8 |
+                                    health == 0), NA_real_, health)) %>%
+  mutate(helpsick = dplyr::if_else((helpsick >= 8 |
+                                      helpsick == 0), NA_real_, helpsick))
+
+## Review distribution of cleaned responses
+ggplot2::ggplot(data = df) +
+  ggplot2::geom_bar(mapping = aes(x = class))
+
+ggplot2::ggplot(data = df) +
+  ggplot2::geom_bar(mapping = aes(x = health))
+
+ggplot2::ggplot(data = df) +
+  ggplot2::geom_bar(mapping = aes(x = helpsick))
+
+
+## Recode variables with appropriate factor labels
+df <- df %>%
+  dplyr::mutate(
+    class = class %>% forcats::as_factor() %>%
+      forcats::fct_recode(
+        "Lower" = "1" ,
+        "Working" = "2",
+        "Middle" = "3",
+        "Upper" = "4"
+      )
+  ) %>%
+  
+  dplyr::mutate(
+    health = health %>% forcats::as_factor() %>%
+      forcats::fct_recode(
+        "Excellent" = "1",
+        "Good" = "2",
+        "Fair" = "3",
+        "Poor" = "4"
+      )
+  )                     
+    
 ## ERROR: the below code didn't relabel anything 
 ## and there are too few labels for the values present. 
 ## We also only need the numeric values for analysis. 
 #GSS$helpsick[GSS$helpsick=="Government help"]<-1
 
-
-GSS$health <- factor(GSS$health, labels = c("Excellent", "Good", "Fair","Poor"))
-GSS$class <- factor(GSS$class, labels = c("Lower", "Working", "Middle", "Upper"))
 
 table(GSS$class, exclude= NULL)
 table(GSS$health, exclude= NULL)
